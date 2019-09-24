226204d9e86d9182356f29073b91bf577d943971
ericsmith92 <ericsmth884@gmail.com>
2019-09-24 16:42:05 -0400

    array conversion solution

--- algorithms/script.js
+++ algorithms/script.js
@@ -71,6 +71,7 @@
 **71. Rating Threshold
 **72. Reflect String
 **73. Sort by Height
+**74. Array Conversion
 */
 
 //1. Remove Adjacent Duplicates From a String (currently only works if string is only composed of adjacent duplicates)
@@ -2497,3 +2498,51 @@ function sortByHeight(arr){
 }
 
 sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]);
+
+//74. Array Conversion
+/*
+Given an array of 2k integers (for some integer k), perform the following operations until 
+the array contains only one element:
+
+-On the 1st, 3rd, and 5th, etc. iterations (1-based) replace each pair of consecutive elements
+with their sum;
+
+-On the 2nd, 4th, 6tn, etc. iterations replace each pair of consecutive elemens with their 
+product. After the algorithm has finished, there will be a single element left in the array.
+return that element.
+
+Ex.
+For inputArray = [1, 2, 3, 4, 5, 6, 7, 8], arrayConversion(inputArray) = 186
+We have -> [1, 2, 3, 4, 5, 6, 7, 8] -> [3, 7, 11, 15] -> [21, 165] -> [186]
+so the answer is 186
+
+Hints
+push()
+*/
+
+function reducer(array, count){
+    const reducedArr = [];
+    if(count % 2 !== 0){
+        for(let i = 0; i < array.length; i+= 2){
+            reducedArr.push(array[i] + array[i + 1]);
+        }
+    }else{
+        for(let i = 0; i < array.length; i+= 2){
+            reducedArr.push(array[i] * array[i + 1]);
+        }
+    }
+    return reducedArr;
+}
+
+function arrayConversion(inputArray){
+    let count = 1;
+
+    while(inputArray.length > 1){
+        inputArray =  reducer(inputArray, count);
+        count++;
+    }
+
+    return inputArray[0];
+}
+
+arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]);
